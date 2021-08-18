bf837a27cf6106b0d7eedab84c408e06622594bc
Berezentsev Evgeny <jonn09@icloud.com>
2021-08-18 16:46:32 +0300

    1. Start setup

---
+++ Watcher/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Watcher
+//
+//  Created by Евгений Березенцев on 18.08.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+
+
+}
+
