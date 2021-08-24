04d7973c13c78a5297fe371542f13c6f053f4c57
Berezentsev Evgeny <jonn09@icloud.com>
2021-08-24 20:02:19 +0300

    NetworkManager added

---
+++ Watcher/Services/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  Watcher
+//
+//  Created by Евгений Березенцев on 24.08.2021.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
--- Watcher/ViewControllers/MainViewController.swift
+++ Watcher/ViewControllers/MainViewController.swift
@@ -16,6 +16,7 @@ class MainViewController: UIViewController {
         tabBarItem.title = "Trending movies"
     }
 
+    
 
 }
 
